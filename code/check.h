2437e6398c515a144ed174152ebe2aee4fb559ef
이정현 <jeonghyun0126@outlook.com>
2019-12-02 01:42:47 +0900

    update_movable_positions 시그니처 변경

--- code/check.h
+++ code/check.h
@@ -2,6 +2,6 @@
 #include "common.h"
 #endif
 
-void update_movable_positions(chess_piece *pieces[NUM_CHESS_PIECES]);
+void update_movable_positions(chess_piece [NUM_CHESS_PIECES]);
 void reset(chess_piece *[NUM_CHESS_PIECES],int [][12], int);
 int promotion(chess_piece *,int);
